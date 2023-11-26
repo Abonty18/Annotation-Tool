202267448ba40a3dbb77668577f5c00b622edd43
Abonty18 <labibafarah2998@gmail.com>
2023-11-26 11:12:58 +0600

    trying for vercel5

--- build_files.sh
+++ build_files.sh
@@ -3,34 +3,19 @@
 # Exit in case of error
 set -e
 
-# Get the current directory of the script
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-# Create a virtual environment if it doesn't exist
-if [ ! -d "$SCRIPT_DIR/venv" ]; then
-  python3 -m venv "$SCRIPT_DIR/venv"
-fi
-
-# Activate the virtual environment using an absolute path
-source "$SCRIPT_DIR/venv/bin/activate"
+# Ensure pip is available
+python3 -m ensurepip
 
 # Upgrade pip to the latest version
-pip install --upgrade pip
+python3 -m pip install --upgrade pip
 
-# Install Python dependencies in the virtual environment
+# Install Python dependencies directly without a virtual environment
 pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput
 
-# After installing dependencies, list them
+# List installed dependencies for debugging purposes
 pip freeze
 
-# Print the current Python path
-echo $PYTHONPATH
-
-# Print the location of the Django package
-python -c "import django; print(django.__path__)"
-
-# Deactivate the virtual environment
-deactivate
+# No need to deactivate the virtual environment since we're not using one
