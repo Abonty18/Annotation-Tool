caa911f5879b67f4ec08ce7ad866042304af0d12
Abonty18 <labibafarah2998@gmail.com>
2023-11-26 11:31:12 +0600

    trying for vercel9

--- build_files.sh
+++ build_files.sh
@@ -1,27 +1,28 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Exit in case of error
-set -e
+# # Exit in case of error
+# set -e
 
-# Ensure pip is available
-python3 -m ensurepip
+# # Ensure pip is available
+# python3 -m ensurepip
 
-# Install the sqlite3 package
-python3 -m pip install sqlite3
+# # Install the sqlite3 package
+# python3 -m pip install sqlite3
 
-# Upgrade pip to the latest version
-# Upgrade pip to the latest version
-python3 -m pip install --upgrade pip
-
+# # Upgrade pip to the latest version
+# # Upgrade pip to the latest version
+# python3 -m pip install --upgrade pip
+echo "BUILD START"
 # Install Python dependencies directly without a virtual environment
-python3 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 
 # Collect static files
-python3 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
+echo "BUILD END"
 
-# List installed dependencies for debugging purposes
-python3 -m pip freeze
+# # List installed dependencies for debugging purposes
+# python3 -m pip freeze
 
-# Print the current Python version for debugging
-python3 --version
+# # Print the current Python version for debugging
+# python3 --version
