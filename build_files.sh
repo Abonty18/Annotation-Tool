b3cb2b3745018000cda41af7ed32c8409bc0e2d5
Abonty18 <labibafarah2998@gmail.com>
2023-11-26 10:58:37 +0600

    trying for vercel2

--- build_files.sh
+++ build_files.sh
@@ -1,27 +1,25 @@
-#!/bin/bash
-echo "BUILD START"
+
+
 # Exit in case of error
 set -e
 
-# Install system dependencies, for example, Python 3.9 and PostgreSQL
-# This is just an example, as Vercel's build environment already includes Python
+# Create a virtual environment
+python3 -m venv venv
 
-# Install Python dependencies
+# Activate the virtual environment
+source venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
 
-# Collect static files into the 'staticfiles' directory
-python manage.py collectstatic --no-input --clear
+# Collect static files
+python manage.py collectstatic --noinput
 
-# Apply migrations (Not recommended for production, handle migrations separately)
+# Apply migrations (optional and not recommended for production)
 # python manage.py migrate
 
-# Create a directory for static files build
-mkdir -p staticfiles_build
-
-# Move collected static files to the static files build directory
-mv staticfiles/* staticfiles_build/
-
-# Clean up
-rm -rf staticfiles
-
-# Note: Additional steps might be required depending on your project's needs
+# Deactivate the virtual environment
+deactivate
