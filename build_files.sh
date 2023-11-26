951474ae138ca5e6cf8674d3ff7b644fd86d3943
Abonty18 <labibafarah2998@gmail.com>
2023-11-26 11:04:16 +0600

    trying for vercel3

--- build_files.sh
+++ build_files.sh
@@ -1,25 +1,36 @@
-
+#!/bin/bash
 
 # Exit in case of error
 set -e
 
-# Create a virtual environment
-python3 -m venv venv
+# Create a virtual environment if it doesn't exist
+if [ ! -d "venv" ]; then
+  python3 -m venv venv
+fi
 
 # Activate the virtual environment
 source venv/bin/activate
 
-# Upgrade pip
+# Upgrade pip to the latest version
 pip install --upgrade pip
 
-# Install dependencies
+# Install Python dependencies in the virtual environment
 pip install -r requirements.txt
 
 # Collect static files
 python manage.py collectstatic --noinput
 
-# Apply migrations (optional and not recommended for production)
-# python manage.py migrate
+# After installing dependencies, list them
+pip freeze
+
+# Print the current Python path
+echo $PYTHONPATH
+
+# Print the location of the Django package
+python -c "import django; print(django.__path__)"
+
 
 # Deactivate the virtual environment
 deactivate
+
+
